7e6ec86afe846db037827de331dc8a50e19465a4
Nobsmoke123 <donaldakobundu@gmail.com>
2022-10-12 23:38:17 +0100

    cleaning up.

--- tests/routes.test.ts
+++ tests/routes.test.ts
@@ -5,7 +5,7 @@ import dotevn from 'dotenv';
 // Initialize configuration
 dotevn.config();
 
-describe('Test the welcome page', () => {
+describe('Test / Endpoint', () => {
   test(`Should return the text 'Welcome to >>> IQAir API.'`, async () => {
     const res = await request(app).get('/').send();
     expect(res.text).toEqual('Welcome to >>> IQAir API.');
@@ -13,12 +13,18 @@ describe('Test the welcome page', () => {
   });
 });
 
-describe('Test the nearest city endpoint.', () => {
+describe('Nearest city====Endpoint', () => {
   test('Should return an object with a key of Result', async () => {
+    jest.setTimeout(10000);
     const lat = 40.73061;
     const long = -73.935242;
     const res = await request(app).get(`/nearest-city/${lat}/${long}`);
-    expect(res.body).toHaveProperty('Result');
     expect(res.statusCode).toEqual(200);
+
+    expect(res.body.Result.Pollution).toHaveProperty('maincn');
+    expect(res.body.Result.Pollution).toHaveProperty('aqicn');
+    expect(res.body.Result.Pollution).toHaveProperty('mainus');
+    expect(res.body.Result.Pollution).toHaveProperty('aqius');
+    expect(res.body.Result.Pollution).toHaveProperty('ts');
   });
 });
